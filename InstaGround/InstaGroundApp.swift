6ba7545a8b5aa4342ab0a4d48d7e2bedbc019843
elizeurs <13ders@gmail.com>
2021-03-05 16:44:31 -0300

    Initial Commit

---
+++ InstaGround/InstaGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaGroundApp.swift
+//  InstaGround
+//
+//  Created by Elizeu RS on 05/03/21.
+//
+
+import SwiftUI
+
+@main
+struct InstaGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
