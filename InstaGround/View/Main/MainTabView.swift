6fc589b0016670dfd4072e3c3a8ccaead66442bc
elizeurs <13ders@gmail.com>
2021-03-06 13:32:32 -0300

    building mainTabView and feedView

---
+++ InstaGround/View/Main/MainTabView.swift
@@ -0,0 +1,50 @@
+//
+//  MainTabView.swift
+//  InstaGround
+//
+//  Created by Elizeu RS on 05/03/21.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+      TabView {
+        Text("Feed")
+          .tabItem {
+            Image(systemName: "house")
+            Text("Home")
+          }
+        
+        Text("Explore")
+          .tabItem {
+            Image(systemName: "magnifyingglass")
+            Text("Home")
+          }
+        
+        Text("New Post")
+          .tabItem {
+            Image(systemName: "plus.app")
+            Text("Post")
+          }
+        
+        Text("Notifications")
+          .tabItem {
+            Image(systemName: "heart")
+            Text("Notifications")
+          }
+        
+        Text("Profile")
+          .tabItem {
+            Image(systemName: "person")
+            Text("Profile")
+          }
+      }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
