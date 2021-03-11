37963821e545d3d5467efa80196464eced5c6520
elizeurs <13ders@gmail.com>
2021-03-11 13:41:15 -0300

    implement profileActionButton

---
+++ InstaGround/View/Profile/ProfileActionButton.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileActionButton.swift
+//  InstaGround
+//
+//  Created by Elizeu RS on 10/03/21.
+//
+
+import SwiftUI
+
+struct ProfileActionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileActionButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileActionButton()
+    }
+}
