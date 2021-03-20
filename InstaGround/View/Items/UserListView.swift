9e740fd50cabd45b5157aaacb58d662b90cc5c14
elizeurs <13ders@gmail.com>
2021-03-19 21:57:45 -0300

    filtering users with searchBar

--- InstaGround/View/Items/UserListView.swift
+++ InstaGround/View/Items/UserListView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct UserListView: View {
   @ObservedObject var viewModel: SearchViewModel
+  @Binding var searchText: String
+  
+  var users: [User] {
+    return searchText.isEmpty ? viewModel.users : viewModel.filteredUsers(searchText)
+  }
   
     var body: some View {
       ScrollView() {
         LazyVStack(alignment: .leading, spacing: 20) {
 //          ForEach(0 ..< 10) { item in
 //          ForEach(viewModel.users) { _ in
-          ForEach(viewModel.users) { user in
+//          ForEach(viewModel.users) { user in
+          ForEach(users) { user in
             NavigationLink(
               destination: ProfileView(),
               label: {
