d8d46919575b9685889b9724c833d0dd9f0a5843
elizeurs <13ders@gmail.com>
2021-03-22 13:41:50 -0300

    implement fun unfollow and check to see if a use is followed

--- InstaGround/ViewModel/Profile/ProfileViewModel.swift
+++ InstaGround/ViewModel/Profile/ProfileViewModel.swift
@@ -25,10 +25,18 @@ class ProfileViewModel: ObservableObject {
   }
   
   func unfollow() {
-    print("DEBUG: Unfollow user...")
+//    print("DEBUG: Unfollow user...")
+    guard let uid = user.id else { return }
+    UserService.unfollow(uid: uid) { _ in
+      self.user.isFollowed = false
+    }
   }
   
   func checkIfUserIsFollowed() {
-    
+    guard !user.isCurrentUser else { return }
+    guard let uid = user.id else { return }
+    UserService.checkIfUserIsFollowed(uid: uid) { isFollowed in
+      self.user.isFollowed = isFollowed
+    }
   }
 }
